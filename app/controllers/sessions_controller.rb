cec60cccf68bfa05401a60d57339c6afa81e261e
Nicholas Rodriguez <niko876@hotmail.com>
2019-09-04 16:43:28 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,19 @@ class SessionsController < ApplicationController
 
 	def create
 
+		if params[:name].nil?
+      redirect_to '/login'
+    elsif params[:name] == ""
+      redirect_to '/login'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+
 	end
 
 	def destroy
-		
+		session.delete :name
 	end
 
 end
